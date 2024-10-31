5175612582438d4b5d99d44273cfa7cc816036a6
matheusgoissouza <matheus8gois@gmail.com>
2024-10-31 20:24:53 -0300

    Alteração da dimensão do Square e alteração das cores

---
+++ prj01/src/components/Board.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Square from './Square';
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status = winner ? 'Vencedor: ' + winner : 'Próximo: ' + (xIsNext ? 'Xbox' : 'PlayStation');
+
+  return (
+    <>
+      <div className="statuss">{status}</div>
+      {[0, 1, 2].map(row => (
+        <div className="board-row" key={row}>
+          {[0, 1, 2].map(col => (
+            <Square key={3 * row + col} value={squares[3 * row + col]} onSquareClick={() => handleClick(3 * row + col)} />
+          ))}
+        </div>
+      ))}
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Board;
